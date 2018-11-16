2fc89691cbe80e7276b5b5f1c00687d7bc97180c
conor-ryan-quinn <conor.ryan.quinn@gmail.com>
2018-11-16 18:21:01 -0300

    working

--- MiniParsec.hs
+++ MiniParsec.hs
@@ -1,71 +1,79 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 
-module MiniParsec where
+module MiniParsec (
+  Parser,
+  (<|>),
+  number,
+  chainl1,
+  parens,
+  reserved,
+  runParser,
+
+
+) where
+
 
 import Data.Char
 import Control.Monad
 import Control.Applicative
 
-newtype Parser a = Parser { parse :: String -> [(a, String)] }
-
-instance Functor Parser where
-  fmap f (Parser g) = Parser (\s -> let [(a, s1)] = g s
-                                      in [(f a, s1)])
-
-instance Applicative Parser where
-  pure a = Parser (\s -> [(a, s)])
-  (<*>) (Parser sab) (Parser fa) =  
-    Parser (\s -> let [(a, s1)] = fa s 
-                      [(fab, s2)] = sab s1
-                    in [(fab a, s2)])
-
-instance Monad Parser where
-  return = pure
-  (>>=) p f = Parser $ \s -> concatMap (\(a, s') -> parse (f a) s') $ parse p s
-
-instance MonadPlus Parser where
-  mzero = Parser (\cs -> [])
-  mplus p q = Parser (\s -> parse p s ++ parse q s)
-
-instance Alternative Parser where
-  empty = mzero
-  (<|>) p q = Parser $ \s ->
-    case parse p s of
-      [] -> parse q s
-      res -> res
+newtype Parser a = Parser { parse :: String -> [(a,String)] }
 
 runParser :: Parser a -> String -> a
 runParser m s =
   case parse m s of
     [(res, [])] -> res
-    [(_, rs)] -> error "Parser did not consume entire stream."
-    _ -> error "Parser error."
+    [(_, _)]   -> error "Parser did not consume entire stream."
+    _           -> error "Parser error."
 
 item :: Parser Char
 item = Parser $ \s ->
   case s of
-    [] -> []
-    (c:cs) -> [(c, cs)]
+    []     -> []
+    (c:cs) -> [(c,cs)]
 
--- Another version of "some" and "many" in Control.Applicative
-some' :: (Alternative f) => f a -> f [a]
-some' v = some_v
-  where
-    many_v = some_v <|> pure []
-    some_v = (:) <$> v <*> many_v
+bind :: Parser a -> (a -> Parser b) -> Parser b
+bind p f = Parser $ \s -> concatMap (\(a, s') -> parse (f a) s') $ parse p s
 
-many' :: (Alternative f) => f a -> f [a]
-many' v = many_v
-  where
-    many_v = some_v <|> pure []
-    some_v = (:) <$> v <*> many_v
+unit :: a -> Parser a
+unit a = Parser (\s -> [(a,s)])
+
+instance Functor Parser where
+  fmap f (Parser cs) = Parser (\s -> [(f a, b) | (a, b) <- cs s])
+
+instance Applicative Parser where
+  pure = return
+  (Parser cs1) <*> (Parser cs2) = Parser (\s -> [(f a, s2) | (f, s1) <- cs1 s, (a, s2) <- cs2 s1])
+
+instance Monad Parser where
+  return = unit
+  (>>=)  = bind
+
+instance MonadPlus Parser where
+  mzero = failure
+  mplus = combine
+
+instance Alternative Parser where
+  empty = mzero
+  (<|>) = option
+
+combine :: Parser a -> Parser a -> Parser a
+combine p q = Parser (\s -> parse p s ++ parse q s)
+
+failure :: Parser a
+failure = Parser (\cs -> [])
+
+option :: Parser a -> Parser a -> Parser a
+option  p q = Parser $ \s ->
+  case parse p s of
+    []     -> parse q s
+    res    -> res
 
 satisfy :: (Char -> Bool) -> Parser Char
-satisfy p = item >>= \c ->
-  if p c 
-  then return c
-  else (Parser (\cs -> []))
+satisfy p = item `bind` \c ->
+  if p c
+  then unit c
+  else failure
 
 oneOf :: [Char] -> Parser Char
 oneOf s = satisfy (flip elem s)
@@ -77,28 +85,27 @@ chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
 p `chainl1` op = do {a <- p; rest a}
   where rest a = (do f <- op
                      b <- p
-                     b <- p
                      rest (f a b))
                  <|> return a
 
 char :: Char -> Parser Char
-char c = satisfy (c==)
+char c = satisfy (c ==)
 
 natural :: Parser Integer
-natural = read <$> some' (satisfy isDigit)
+natural = read <$> some (satisfy isDigit)
 
 string :: String -> Parser String
 string [] = return []
-string (c:cs) = do { char c; string cs; return (c:cs) }
+string (c:cs) = do { char c; string cs; return (c:cs)}
 
 token :: Parser a -> Parser a
-token p = do { a <- p; spaces; return a}
+token p = do { a <- p; spaces; return a }
 
 reserved :: String -> Parser String
 reserved s = token (string s)
- 
+
 spaces :: Parser String
-spaces = many' $ oneOf " \n\r"
+spaces = many $ oneOf " \n\r"
 
 digit :: Parser Char
 digit = satisfy isDigit
@@ -106,7 +113,7 @@ digit = satisfy isDigit
 number :: Parser Int
 number = do
   s <- string "-" <|> return []
-  cs <- some' digit
+  cs <- some digit
   return $ read (s ++ cs)
 
 parens :: Parser a -> Parser a
